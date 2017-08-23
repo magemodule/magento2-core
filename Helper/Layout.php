c8b3ff2aafd1e0e9c2d495618fef4479f429f10a
Shawn Abramson <sabramson@optaros.com>
2017-08-23 00:12:47 -0400

    Upgraded to version 1.0.4. Added Layout helper class

---
+++ Helper/Layout.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace MageModule\Core\Helper;
+
+class Layout extends \Magento\Framework\App\Helper\AbstractHelper
+{
+    /**
+     * @var \Magento\Framework\App\ViewInterface
+     */
+    private $view;
+
+    /**
+     * Layout constructor.
+     *
+     * @param \Magento\Framework\App\Helper\Context $context
+     * @param \Magento\Framework\App\ViewInterface  $view
+     */
+    public function __construct(
+        \Magento\Framework\App\Helper\Context $context,
+        \Magento\Framework\App\ViewInterface $view
+    ) {
+        parent::__construct($context);
+        $this->view = $view;
+    }
+
+    /**
+     * @return \Magento\Framework\App\ViewInterface
+     */
+    public function getView()
+    {
+        return $this->view;
+    }
+
+    /**
+     * @return \Magento\Framework\View\LayoutInterface
+     */
+    public function getLayout()
+    {
+        return $this->getView()->getLayout();
+    }
+
+    /**
+     * @return \Magento\Framework\View\Layout\ProcessorInterface
+     */
+    public function getUpdate()
+    {
+        return $this->getLayout()->getUpdate();
+    }
+
+    /**
+     * @return array
+     */
+    public function getHandles()
+    {
+        return $this->getUpdate()->getHandles();
+    }
+
+    /**
+     * @param string $handle
+     *
+     * @return bool
+     */
+    public function isCurrentHandle($handle)
+    {
+        return in_array($handle, $this->getHandles());
+    }
+}

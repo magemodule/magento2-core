4c4275fb7c8d54a0dd5ee80ab1f3d54bb3382995
Shawn Abramson <sabramson@optaros.com>
2018-07-31 01:22:51 -0400

    Added an abstract attribute collection class

---
+++ Model/ResourceModel/Entity/Attribute/AbstractCollection.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace MageModule\Core\Model\ResourceModel\Entity\Attribute;
+
+use Magento\Framework\Exception\LocalizedException;
+
+abstract class AbstractCollection extends \Magento\Eav\Model\ResourceModel\Entity\Attribute\Collection
+{
+    /**
+     * @var string
+     */
+    protected $entityTypeCode;
+
+    /**
+     * @return void
+     */
+    protected function _construct()
+    {
+        parent::_construct();
+
+        $this->_itemObjectClass = Attribute::class;
+    }
+
+    /**
+     * @return $this
+     * @throws LocalizedException
+     */
+    protected function _initSelect()
+    {
+        $select = $this->getSelect()->from(
+            ['main_table' => $this->getResource()->getMainTable()]
+        );
+
+        $select->join(
+            ['entity_type_table' => $this->getResource()->getTable('eav_entity_type')],
+            'main_table.entity_type_id = entity_type_table.entity_type_id',
+            ''
+        );
+
+        $select->where('entity_type_table.entity_type_code = ?', $this->entityTypeCode);
+
+        return $this;
+    }
+}

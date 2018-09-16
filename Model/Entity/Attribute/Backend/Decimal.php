ef2027e3e0ce49fcf7caad29c235a6d9131d9c73
Shawn Abramson <sabramson@optaros.com>
2018-09-15 21:50:43 -0400

    Updated copyright notice in all files

--- Model/Entity/Attribute/Backend/Decimal.php
+++ Model/Entity/Attribute/Backend/Decimal.php
@@ -4,7 +4,7 @@
  *
  * LICENSE: This source file is subject to our standard End User License
  * Agreeement (EULA) that is available through the world-wide-web at the
- * following URI: https://www.magemodule.com/magento2-ext-license.html.
+ * following URI: https://www.magemodule.com/end-user-license-agreement/.
  *
  *  If you did not receive a copy of the EULA and are unable to obtain it through
  *  the web, please send a note to admin@magemodule.com so that we can mail
@@ -12,7 +12,7 @@
  *
  *  @author        MageModule admin@magemodule.com
  *  @copyright    2018 MageModule, LLC
- *  @license        https://www.magemodule.com/magento2-ext-license.html
+ *  @license        https://www.magemodule.com/end-user-license-agreement/
  */
 
 namespace MageModule\Core\Model\Entity\Attribute\Backend;
